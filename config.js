c61d5235d0469562ecced01d9a27508642bbb847
weddy1225-blip <weddy1225@gmail.com>
2026-07-30 14:02:48 +0800

    Connect secure score backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // 部署後只需填入 Apps Script 網頁應用程式網址。此檔不得放置任何 API 金鑰。
 window.EXAM_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbyYLM5NqLvyqYntZGqswCWURDNsuC1d8gZmzMmYV8OmH3wqzrD_9GKD5F5UNolNuOPGLg/exec",
   examId: "HST-20260730-A",
   durationMinutes: 20
 };
